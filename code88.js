0ba65d74f554ca90fa20e99f3f7ce4010fedd983
kdudziak <kdudziak@fkbl.com>
2016-05-25 17:13:57 -0400

    solve  prob 88

---
+++ code88.js
@@ -0,0 +1,64 @@
+var _ = require('underscore')
+
+function sortNum(a,b) { return b - a; }
+var add = function(a,b){return parseInt(a)+parseInt(b)};
+var prod = function(a,b){return a*b};
+var p = require('./primes');
+var primes = {};
+p.map(function(a){primes[a]=true})
+primes[1]=true;
+
+var factorize = function(n){
+  if(primes[n]){ return [];}
+  var factors= [];
+  for (var i = 2; i <= n/i; i++) {
+    if( n%i=== 0){
+      factors.push([n/i,i])
+    }
+  }
+  return factors
+}
+
+function allFactors(n){
+  var stack = [ [n] ];
+  var added = {};
+  var factors = [ ];
+  while (stack.length > 0){
+    var current = stack.shift();
+    for (var i = 0; i < current.length; i++) {
+      var all = factorize(current[i]);
+      for (var j = 0; j < all.length; j++) {
+         var temp = _.clone(current);
+         temp[i] = all[j];
+         temp = _.flatten(temp).sort(sortNum);
+         if(!added[temp]){
+           stack.push(temp);
+           factors.push(temp);
+           added[temp] = true;
+         }
+      }
+    }
+  }
+  return factors
+}
+
+
+
+var k = {2:4};
+for (var i = 4; i <100000; i++) {
+  console.log(i)
+ var vals = allFactors(i).map(function(a){return a.length - a.reduce(add) + i; })
+ for (var j = 0; j < vals.length; j++) {
+   if( (k[vals[j]]||Infinity) > i){
+     k[vals[j]] = i;
+   }
+ }
+}
+var obj = {};
+for(var z=2;z<12001;z++){
+  obj[k[z]]= true;
+}
+
+console.log(Object.keys(obj))
+console.log(Object.keys(obj).length)
+console.log(Object.keys(obj).reduce(add))
